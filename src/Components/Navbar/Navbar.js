5eb43378818f0f747be7a5f00721433c3e7c8877
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-30 00:08:29 +0600

    tailwind diye clicky navigation banalam

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
     const routes = [
-        {id: 1, name: 'Home', link: '/home'},
-        {id: 2, name: 'Shop', link: '/shop'},
-        {id: 3, name: 'Coupons', link: '/coupons'}
+        { id: 1, name: 'Home', link: '/home' },
+        { id: 2, name: 'Shop', link: '/shop' },
+        { id: 3, name: 'Coupons', link: '/coupons' },
+        { id: 3, name: 'Contact', link: '/contact' }
     ]
-    
+
     return (
-       <nav>
-           <ul>
-              {
-                  routes.map(route => <Link
-                  key={route.id}
-                  route={route}
-                  ></Link>)
-              }
-           </ul>
-       </nav>
+        <nav>
+            <div className='w-6 h-6 md:hidden' onClick={() => setOpen(!open)}>
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+            <ul className={`md:flex justify-center absolute duration-500 ease-in ${open ? 'top-6' :'top-[-120px]'}`}>
+                {
+                    routes.map(route => <Link
+                        key={route.id}
+                        route={route}
+                    ></Link>)
+                }
+            </ul>
+        </nav>
     );
 };
 
